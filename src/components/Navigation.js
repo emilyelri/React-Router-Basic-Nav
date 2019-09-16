4309c038c503b5ab055e181cd730903daca33169
Emily_Richard <richard.emily.e@gmail.com>
2019-09-16 15:56:03 -0400

    Set up basic routing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom';
+
+import Home from "./Home";
+import About from "./About"
+import Contact from "./Contact";
 
 const Navigation = () => {
+
   return (
+
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link to="/">Home</Link>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to="/about">About</Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to="/contact">Contact</Link>
         </div>
       </div>
+      <Route exact path="/" components = {Home} />
+      <Route path="/about" components = {About} />
+      <Route path="/contact" components = {Contact} />
     </div>
   );
 };
